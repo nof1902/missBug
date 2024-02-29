2ba9573b3b37ad6ba1ccc930fc8cca589c9df3e6
nof1902 <nof1902@gmail.com>
2024-02-29 13:09:06 +0200

    connection to DB

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: 'mongodb+srv://nofar:H0FWCmxC7EGFJXoD@cluster0.ivrgfmo.mongodb.net/',
+    dbURL: 'mongodb+srv://nofar:H0FWCmxC7EGFJXoD@cluster0.ivrgfmo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     dbName: 'car_db'
 }
