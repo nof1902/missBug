751f138654dcf02478e42eb78dc76b081cecebd9
nof1902 <nof1902@gmail.com>
2024-02-29 13:15:45 +0200

    connection to DB

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 export default {
-    dbURL: 'mongodb://127.0.0.1:27017',
-    // dbURL: 'mongodb+srv://nofar:H0FWCmxC7EGFJXoD@cluster0.ivrgfmo.mongodb.net/',
+    // dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: 'mongodb+srv://nofar:H0FWCmxC7EGFJXoD@cluster0.ivrgfmo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     dbName: 'missBug_db'
 }
